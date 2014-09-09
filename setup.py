f20bd9a49e31fe7c2fdb2999f4515b7cec3dd9fe
Leila Zilles <lzilles@cs.washington.edu>
2014-09-09 13:44:59 -0400

    remove extra gunk from setup

--- setup.py
+++ setup.py
@@ -6,11 +6,7 @@ setup(
     author='thricedotted',
     author_email='thricedotted@gmail.com',
     packages=['twitterbot'],
-    #scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
-    #url='http://pypi.python.org/pypi/TowelStuff/',
-    #license='LICENSE.txt',
     description='A simple Python framework for creating Twitter bots.',
-    #long_description=open('README.txt').read(),
     install_requires=[
         "tweepy >= 2.3"
     ],
